ae9b17a079153879ec81a33f7b0f3342c44294eb
SAINATH DUTKAR <31874079+SainathDutkar@users.noreply.github.com>
2018-07-24 23:56:46 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-javac ./src/FileReader.java
-java -cp ./src FileReader ./input/itcont.txt ./output/top_cost_drug.txt
+cd src
+javac -d .  FileReader.java
+java FileReader ../input/itcont.txt ../output/top_cost_drug.txt
