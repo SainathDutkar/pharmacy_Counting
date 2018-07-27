9f8e2cfbfead20c4f84f2921950f3ee012c43513
SAINATH DUTKAR <31874079+SainathDutkar@users.noreply.github.com>
2018-07-27 12:13:18 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,3 +3,7 @@
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
 #python ./src/pharmacy_counting.py ./input/itcont.txt ./output/top_cost_drug.txt
+
+javac src/PharmacyCount.java
+cd src
+java PharmacyCount ../input/itcont.txt ../output/top_cost_drug.txt
